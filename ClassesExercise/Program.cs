4717f3d9544e005b9c1e5a259af68e78b0078cfd
Valdir Lima <valdirlima508@gmail.com>
2022-02-21 15:49:05 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var myFirstCar = new Car();
+
+            myFirstCar.Make = "Honda";
+            myFirstCar.Model = "Accord";
+            myFirstCar.Year = 2020;
+
+            Console.WriteLine($"Make: {myFirstCar.Make}");
+            Console.WriteLine($"Model: {myFirstCar.Model}");
+            Console.WriteLine($"Year: {myFirstCar.Year}");
+            
+        }
+    }
+}
